7f665a44d2da1d8d556b097309e244f5a7ebf011
aso1901102 <1901102@s.asojuku.ac.jp>
2019-12-02 12:45:09 +0900

    ver1.0

---
+++ app/src/main/java/jp/ac/asojuku/myslideshow/ImageFragment.kt
@@ -0,0 +1,50 @@
+package jp.ac.asojuku.myslideshow
+
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+/**
+ * A simple [Fragment] subclass.
+ */
+
+//グローバルな変数として追加
+val IMG_RES_ID = "IMG_RES_ID";
+
+class ImageFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_image, container, false)
+    }
+
+    //companion objectを使ってクラスにStaticメソッドを実装
+    companion object{
+        //Static メソッドを定義
+        fun newInstance(imageResourseId: Int): ImageFragment {
+            //Bundleクラスの変数を作る（情報をまとめて保管できるクラス）
+            val bundle = Bundle()
+
+            //バンドル変数に画像のリソースIDを保存する
+            //キーワードと値（画像リソースID）
+            bundle.putInt("IMG_RES_ID",imageResourseId);
+
+            //自分の方のインスタンスを作る
+            val imageFragment = ImageFragment();
+
+            //生成したインスタンスのargumentプロパティにbundle情報を設定しておく
+            imageFragment.arguments = bundle;
+
+            //生成したインスタンスをリターンする
+            return imageFragment;
+
+        }
+    }
+
+}
